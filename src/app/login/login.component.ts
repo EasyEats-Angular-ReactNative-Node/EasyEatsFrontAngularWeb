f6e3bcbc33b482dcc776b54bf4b207103e670409
Caique Matos <caique-matos@outlook.com.br>
2023-10-22 18:37:41 -0300

    Tirei a função login do login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,37 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  login() {
-    const email = document.getElementById("email") as HTMLInputElement;
-    const senha = document.getElementById("senha") as HTMLInputElement;
-
-    const dados = {
-      email,
-      senha,
-    };
-
-    fetch("http://localhost:3000/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dados),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Requisição realizada com sucesso.");
-        console.log(data);
-
-        if (data.status === "sucesso") {
-          alert("Login realizado com sucesso!");
-        } else {
-          alert("Usuário ou senha inválidos.");
-        }
-      })
-      .catch((error) => {
-        console.log("Erro ao realizar requisição.");
-        console.log(error);
-      });
-  }
+ 
 
 }
