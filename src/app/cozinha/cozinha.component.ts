b5d32d94bedde248d022b1d3d32efe60d9de4fbf
PedroRodante <pedrorodantevicente@gmail.com>
2023-11-17 07:16:43 -0300

    ATT

--- src/app/cozinha/cozinha.component.ts
+++ src/app/cozinha/cozinha.component.ts
@@ -5,29 +5,75 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-cozinha',
   templateUrl: './cozinha.component.html',
-  styleUrls: ['./cozinha.component.css']
+  styleUrls: ['./cozinha.component.css'],
 })
 export class CozinhaComponent {
-
   constructor(private router: Router) {
     this.pedidos = [];
   }
   pedidos: any[];
 
   ngOnInit() {
+    this.atualizarDados();
+  }
+
+  atualizarDados() {
     this.Pedidos();
+
+    setInterval(() => {
+      this.Pedidos();
+    }, 5000); // Atualiza a cada 5 segundos
   }
 
   Pedidos() {
+    console.log('Iniciei a busca dos pedidos');
+
     let id_restaurante = localStorage.getItem('id');
 
-    $.post(`https://kwr3pd-4200.csb.app/cardapio`,
-    {
-      id_restaurante: id_restaurante
-    }, (res) => {
-      console.log("Recebi alguma coisa");
-      console.log(res);
-      this.pedidos = res;
-    });
+    $.post(
+      `https://kwr3pd-4200.csb.app/cozinhaWeb`,
+      {
+        id_restaurante: id_restaurante,
+      },
+      (res) => {
+        console.log('Recebi alguma coisa');
+        console.log(res);
+        this.pedidos = res;
+        this.BuscarNomeMesa();
+      }
+    );
+  }
+
+  BuscarNomeMesa() {
+    console.log('Iniciei a busca dos nomes das mesas');
+
+    for (let index = 0; index < this.pedidos.length; index++) {
+      console.log(this.pedidos[index].id_mesa);
+      $.post(
+        `https://kwr3pd-4200.csb.app/mesaNome`,
+        {
+          id_mesa: this.pedidos[index].id_mesa,
+        },
+        (res) => {
+          console.log(res[0].nome);
+          this.pedidos[index].nomeMesa = res[0].nome;
+        }
+      );
+    }
+  }
+
+  Entregar(id: string) {
+    console.log('Entregando Pedido ao Cliente');
+
+    $.post(
+      `https://kwr3pd-4200.csb.app/entregarPedido`,
+      {
+        id: id,
+      },
+      (res) => {
+        console.log(res);
+        window.location.reload();
+      }
+    );
   }
 }
